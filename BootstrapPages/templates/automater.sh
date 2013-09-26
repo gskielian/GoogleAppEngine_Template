36b0edf8a64c6c85b7c66341a1afd80dd5acd268
gskielian <gregory.kielian@hackerdojo.com>
2013-09-26 00:35:54 -0700

    Update automater.sh

--- BootstrapPages/templates/automater.sh
+++ BootstrapPages/templates/automater.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
+#ran this script in a bash for loop to change the paths for the local .css file in the index.html of each example
 
  sed 's/'"$1"'.css/..\/..\/assets\/css\/'"$1"'.css/g' index.html > $1/index.html
